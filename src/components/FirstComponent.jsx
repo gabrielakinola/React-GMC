003ba332924cbb9e8f4c3ff38ace5d27dbe7e9bf
gabrielakinola <gabrieltemitopeakinola@gmail.com>
2024-01-13 22:11:55 +0100

    React app

---
+++ src/components/FirstComponent.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./FirstComponent.css";
+
+const FirstComponent = () => {
+  return (
+    <>
+      <div style={{ color: "red" }}>I am a boy</div>
+      <p>ihshxsuxhuh</p>
+      <p className="my-paragraph">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At veniam sed
+        ratione suscipit esse rerum sunt velit, possimus error iure,
+        consequuntur quas soluta. Deserunt distinctio est dolorem atque tempore
+        iusto!
+      </p>
+    </>
+  );
+};
+
+export default FirstComponent;
